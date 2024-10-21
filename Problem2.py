e067bfa2936974182aaf259a706131902b974f82
s-beraz <skylerberazwork@gmail.com>
2024-10-20 20:54:32 -0500

    Update Problem2.py

--- Problem2.py
+++ Problem2.py
@@ -1,4 +1,22 @@
 #Midterm Exam
 #Problem2.py
-#Name:
-#Date:
+#Name: Skye Beraz
+#Date: 10/20/2024
+
+def findVowel(string):
+    string = string.upper()
+    vowelList = "AEIOU"
+    freq = [0,0,0,0,0]
+    for ch in string:
+        position = (vowelList.find(ch))
+        if position != -1:
+            freq[position] += 1
+    output = ""	
+    for i in range(5):
+        print(vowelList[i], ":", freq[i])
+        line = vowelList[i] + "," + str(freq[i]) + "\n"
+        output = output + line
+		
+def main():
+    string = input("Enter a sentence: ")
+    findVowel(string)
